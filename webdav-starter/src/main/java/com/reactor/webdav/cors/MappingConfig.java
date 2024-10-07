4cf4139fe8a1f2c326559a99f40330cc03e75193
alekcei <alekcei_07@mail.ru>
2024-10-08 00:48:51 +0500

    cors

--- webdav-starter/src/main/java/com/reactor/webdav/cors/MappingConfig.java
+++ webdav-starter/src/main/java/com/reactor/webdav/cors/MappingConfig.java
@@ -1,6 +1,7 @@
 package com.reactor.webdav.cors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.support.RouterFunctionMapping;
@@ -11,6 +12,7 @@ import javax.annotation.PostConstruct;
 @ConditionalOnProperty(prefix = "webdav.cors", name = "origins")
 class MappingConfig {
     @Autowired
+    @Qualifier("routerFunctionMapping")
     RouterFunctionMapping mapping;
 
     @PostConstruct
